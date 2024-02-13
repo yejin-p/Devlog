d65a2067ac00efe3d2d94442aeb8f1c8a7a2bbe4
yejin-p <149395946+yejin-p@users.noreply.github.com>
2024-02-13 10:23:07 +0900

    Learn : 리액트 공식 홈페이지 튜토리얼 - 라우팅 추가

--- ReactTuto/src/App.js
+++ ReactTuto/src/App.js
@@ -3,9 +3,15 @@ import Profile from './pages/pass-prop';
 import List from './pages/filter-render';
 import Tree from './pages/tree-struct';
 import Event from './pages/handle-event';
+import Object from './pages/object';
+import Example1 from './pages/example1';
+import Example2 from './pages/example2';
+import Interactive from './pages/interactive-state';
 
 import Home from './pages/home';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+// Tobe... 전역 컴포넌트 관리를 할 수 있다면 좋을 것 같은데... 
+// useContext 또는 Redux와 같은 상태관리 툴을 이용해야할 것 같다
 export default function App() {
   return (
   <>
@@ -17,6 +23,10 @@ export default function App() {
         <Route path="/filter-render" element={<List/>}></Route>
         <Route path="/tree-struct" element={<Tree/>}></Route>
         <Route path="/handle-event" element={<Event/>}></Route>
+        <Route path="/object" element={<Object/>}></Route>
+        <Route path="/example1" element={<Example1/>}></Route>
+        <Route path="/example2" element={<Example2/>}></Route>
+        <Route path="/interactive-state" element={<Interactive/>}></Route>
       </Routes>
     </BrowserRouter>
   </>
